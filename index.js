b59f583f412d250c8bab375f707cfa034a779638
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-04-01 09:55:52 +0500

    changed origin to vercel

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://task6-front-end.vercel.app/',
         methods: '*'
     }
 });
